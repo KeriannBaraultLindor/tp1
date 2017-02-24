5aa2c1560ddc16295b9a5344c87301da3516c008
Keriann Barault-Lindor <keriannbl@hotmail.com>
2017-02-23 20:25:31 -0500

    Essais d'intégration de l'animation de scroll javascript dans la cinquième image SVG. Ne fonctionne pas

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 // on cible l'élément « path » que l'on veut animer 
-	var dessins = document.querySelectorAll("#monSVG path");
+var dessins = document.querySelectorAll("#monSVG path");
 
-	// la longueur du chemin «path»
-	for (var i = 0; i < dessins.length; i++) {
-		// La longueur du tracé
-		longueur = dessins[i].getTotalLength()
+// la longueur du chemin «path»
+for (var i = 0; i < dessins.length; i++) {
+	// La longueur du tracé
+	longueur = dessins[i].getTotalLength()
 
-		dessins[i].style.strokeDasharray = longueur;
-		dessins[i].style.strokeDashoffset = longueur;
-	}
+	dessins[i].style.strokeDasharray = longueur;
+	dessins[i].style.strokeDashoffset = longueur;
+}
 
-	// extraire la fraction du déplacement de la barre de défilement et  ajusté le tracé
-	window.addEventListener("scroll", maTrace);
+// extraire la fraction du déplacement de la barre de défilement et  ajusté le tracé
+window.addEventListener("scroll", maTrace);
 
-	function maTrace() {
-	  var fractionDuScroll = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+function maTrace() {
+  var fractionDuScroll = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
 
-	  for (var i = 0; i < dessins.length; i++) {
-	  	longueur = dessins[i].getTotalLength();
-	  	trace = dessins[i].getTotalLength();
-	  	dessins[i].style.strokeDashoffset = longueur - trace * fractionDuScroll ;
-	  }
-	}
+  for (var i = 0; i < dessins.length; i++) {
+  	longueur = dessins[i].getTotalLength();
+  	trace = dessins[i].getTotalLength();
+  	dessins[i].style.strokeDashoffset = longueur - trace * fractionDuScroll ;
+  }
+}
